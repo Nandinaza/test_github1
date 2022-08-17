61c5b23aeb18b8ae21d5d7dcac40ab2d5c351f3b
Nandi <nandichanel@gmail.com>
2022-08-17 20:21:18 +0700

    projek react

--- java.java
+++ java.java
@@ -1,5 +1,18 @@
 public class java{
        public static void main (String[] args) {
               System.out.println("hello world");
+              main.test();
+       }
+}
+public class main{
+       int a = 50;
+       void test(){
+              int indek[] = new indek[10]
+              try {
+                     System.out.println(indek[99]);
+              } catch(Exception e) {
+                     System.out.println(e);
+              }
+              
        }
 }
