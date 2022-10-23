48ca7b115cd913047c6cedba62c9a48bae8bff2a
nazyza <nandichanel@gmail.com>
2022-10-23 14:43:18 +0700

    nwe

--- java.java
+++ java.java
@@ -1,11 +1,17 @@
+import java.io.IOException;
 public class java{
-       public static void main (String[] args) {
+       public static void main (String[] args)throws IOException {
               System.out.println("hello world");
               main.test();
               
               teman teman1 = new teman("muo");
               teman1.show((byte)0);
               //ini saya tamba
+              
+              ProcessBuilder pb = new ProcessBuilder();
+              pb.command("cd ..");
+              Process pro = pb.start();
+              //String result = read(pro);
        }
 }
 class main{
